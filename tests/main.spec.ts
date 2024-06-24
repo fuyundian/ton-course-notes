8cbc03e70995103ab1aef7366efc7f6cc2968521
符云典 <>
2024-06-24 12:15:05 +0800

    1. 编写存储代码

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -9,11 +9,15 @@ describe("main.fc contract tests", () => {
     it("our first test", async () => {
         const blockchain = await Blockchain.create();
         const cellCode = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+        const initAddress = await blockchain.treasury("initAddress initAddress1");
         const myContract = blockchain.openContract(
-            await MainContract.createFromConfig({}, cellCode)
+            await MainContract.createFromConfig({
+                number: 0,
+                address: initAddress.address,
+            }, cellCode)
         );
-        const senderWallet = await blockchain.treasury("sneder");
-        const sentMessageResult = await myContract.sendInternalMessage(senderWallet.getSender(), toNano("0.05"));
+        const senderWallet = await blockchain.treasury("sender1");
+        const sentMessageResult = await myContract.sendIncrement(senderWallet.getSender(), toNano("0.05"), 1);
         expect(sentMessageResult.transactions).toHaveTransaction({
             from: senderWallet.address,
             to: myContract.address,
@@ -21,6 +25,7 @@ describe("main.fc contract tests", () => {
         });
         const data = await myContract.getData();
         expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+        expect(data.number).toEqual(1);
     });
 
 });
--- wrappers/MainContract.ts
+++ wrappers/MainContract.ts
@@ -1,26 +1,46 @@
-import { Address, beginCell, Cell, Contract, ContractABI, contractAddress, ContractProvider, Sender, SendMode, StateInit } from "@ton/core";
-import { Maybe } from "@ton/core/dist/utils/maybe";
+import { Address, beginCell, Cell, ContractABI, contractAddress, ContractProvider, Sender, SendMode, StateInit } from "@ton/core";
+// import { Maybe } from "@ton/core/dist/utils/maybe";
 
-// export interface Contract {
-//     readonly address: Address;
-//     readonly init?: { code: Cell; data: Cell }
-// }
+export interface Contract {
+    readonly address: Address;
+    readonly init?: { code: Cell; data: Cell }
+}
 
+export type MainContractConfig = {
+    number: number;
+    address: Address;
+};
+
+export function mainContractConfigToCell(config: MainContractConfig): Cell {
+    return beginCell().storeUint(config.number, 32).storeAddress(config.address).endCell();
+}
 
 export class MainContract implements Contract {
     constructor(
-        // readonly address: Address,
-        // readonly init?: { code: Cell; data: Cell }
         readonly address: Address,
-        readonly init?: Maybe<StateInit>,
-        readonly abi?: Maybe<ContractABI>,
+        readonly init?: { code: Cell; data: Cell }
+        // readonly address: Address,
+        // readonly init?: Maybe<StateInit>,
+        // readonly abi?: Maybe<ContractABI>,
     ) {
 
     }
-    static createFromConfig(config: any, code: Cell, wordChain = 0) {
-        const data = beginCell().endCell();
+    // static createFromConfig(config: any, code: Cell, wordChain = 0) {
+    //     const data = beginCell().endCell();
+    //     const init = { code, data };
+    //     const address = contractAddress(wordChain, init);
+    //     return new MainContract(address, init);
+    // }
+
+    static createFromConfig(
+        config: MainContractConfig,
+        code: Cell,
+        workchain = 0
+    ) {
+        const data = mainContractConfigToCell(config);
         const init = { code, data };
-        const address = contractAddress(wordChain, init);
+        const address = contractAddress(workchain, init);
+
         return new MainContract(address, init);
     }
     async sendInternalMessage(
@@ -32,9 +52,30 @@ export class MainContract implements Contract {
     }
 
     async getData(provide: ContractProvider) {
-        const stack = await provide.get("get_the_latest_sender", []);
+        const { stack } = await provide.get("get_contract_storage_data", []);
+        console.log(stack, "stack")
         return {
-            recent_sender: stack.stack.readAddress(),
+            number: stack.readNumber(),
+            recent_sender: stack.readAddress(),
         };
+    }
+
+    async sendIncrement(
+        provider: ContractProvider,
+        sender: Sender,
+        value: bigint,
+        increment_by: number
+    ) {
+
+        const msg_body = beginCell()
+            .storeUint(1, 32) // 操作码
+            .storeUint(increment_by, 32) // 计数器值
+            .endCell();
+
+        await provider.internal(sender, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: msg_body,
+        });
     }
 }
